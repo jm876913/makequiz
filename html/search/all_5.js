64f18e449fc32fdf3a44e6a3a797f3e46186705b
Jacob Mulligan <jmulliga@sp-001.cs.ohiou.edu>
2017-10-12 11:35:37 -0400

    adding html and latex documentation

---
+++ html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['output',['output',['../classcourse.html#afdd5ccf71bbbb7797b75854f16bf373f',1,'course']]]
+];
---
+++ html/search/all_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['remove',['remove',['../class_college.html#a4d2ae513b36e6421fb1ca2c08459cfe6',1,'College']]]
+];
